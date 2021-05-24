1fe3efef2c88fd8866a58f35d4bcd391fe162481
Lucy <lucybellott@gmail.com>
2021-05-24 18:23:13 -0400

    commit tree 5a6860fcc9580b35039bdd5972b3c21fb319143b

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
+
+  //renderBooks(json);
+ 
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +23,8 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+/*fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(json));*/
